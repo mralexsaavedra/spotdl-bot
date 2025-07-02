98777bcd094083499289d087ae384684f2e4909e
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-07-02 15:47:59 +0200

    Update TOKEN_PATH in settings and enhance logging in Spotify authentication; add success message for token refresh in Spanish locale.

--- core/spotify_auth.py
+++ core/spotify_auth.py
@@ -1,5 +1,7 @@
 from config.settings import SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI, TOKEN_PATH
 from core.locale import get_text
+from core.logger import setup_logger
+from core.utils import send_message
 import requests
 import os
 import json
@@ -7,23 +9,29 @@ import time
 import base64
 import urllib.parse
 
-from core.utils import send_message
-
 SCOPES="playlist-read-private user-follow-read user-library-read"
 
+logger = setup_logger(__name__)
+
 def save_token(token_data):
+  logger.debug("Saving token to file")
   token_data['expires_at'] = int(time.time()) + token_data['expires_in']
   os.makedirs(os.path.dirname(TOKEN_PATH), exist_ok=True)
   with open(TOKEN_PATH, 'w') as f:
+    logger.debug(f"Token data to save: {token_data}")
     json.dump(token_data, f)
 
 def load_token():
+  logger.debug("Loading token")
   if os.path.exists(TOKEN_PATH):
     with open(TOKEN_PATH, 'r') as f:
+      logger.debug("Loading token from file")
       return json.load(f)
+  logger.debug("Token file does not exist")
   return None
 
-def refresh_token(refresh_token):
+def refresh_token(bot, refresh_token):
+  logger.debug("Refreshing token")
   token_url = 'https://accounts.spotify.com/api/token'
   auth_header = base64.b64encode(f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}".encode()).decode()
   headers = {
@@ -37,13 +45,17 @@ def refresh_token(refresh_token):
   r = requests.post(token_url, headers=headers, data=data)
   if r.status_code == 200:
     new_token = r.json()
+    logger.debug(f"New token received: {new_token}")
     new_token['refresh_token'] = refresh_token  # mantener el mismo si no se devuelve uno nuevo
     save_token(new_token)
+    send_message(bot, message=get_text("refresh_token_success"))
   else:
     raise Exception(f"❌ Error al refrescar el token: {r.text}")
   
-def get_new_token(message):
+def get_new_token(bot, message):
+  logger.debug("Received new token code")
   code = message.text.strip()
+  logger.debug(f"Authorization code: {code}")
   token_url = 'https://accounts.spotify.com/api/token'
   auth_header = base64.b64encode(f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}".encode()).decode()
   headers = {
@@ -59,12 +71,15 @@ def get_new_token(message):
   r = requests.post(token_url, headers=headers, data=data)
   if r.status_code == 200:
     token = r.json()
+    logger.debug(f"Token received: {token}")
     save_token(token)
+    send_message(bot, message=get_text("authorize_success"))
   else:
     raise Exception(f"❌ Error al obtener token: {r.text}")
    
 
 def authorize(bot, message):
+  logger.debug("Starting authorization process")
   params = {
     'client_id': SPOTIFY_CLIENT_ID,
     'response_type': 'code',
@@ -72,15 +87,17 @@ def authorize(bot, message):
     'scope': SCOPES
   }
   auth_url = f"https://accounts.spotify.com/authorize?{urllib.parse.urlencode(params)}"
+  logger.debug(f"Authorization URL: {auth_url}")
   send_message(bot, message=get_text("authorize", auth_url))
   time.sleep(5)
   send_message(bot, message=get_text("redirect_url"))
-  bot.register_next_step_handler(message, get_new_token)
+  bot.register_next_step_handler(message, get_new_token, bot)
 
 def get_valid_token(bot, message):
   token = load_token()
+  logger.debug(f"Token loaded: {token}")
   if token:
     if not int(time.time()) < token.get('expires_at', 0):
-      refresh_token(token['refresh_token'])
+      refresh_token(bot, refresh_token=token['refresh_token'])
   else:
     authorize(bot, message)
