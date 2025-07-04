0fd8c0e0f1f04f2b0b9b9d7423ded7005bdb5d3f
Alexander Saavedra <mralexsaavedra@gmail.com>
2025-07-04 09:37:24 +0200

    Refactor command handling; improve token validation and enhance command documentation

--- bot/commands.py
+++ bot/commands.py
@@ -1,3 +1,10 @@
+# --- Standard library imports ---
+import time
+
+# --- Third-party imports ---
+import telebot
+
+# --- Project imports ---
 from config.config import VERSION
 from core.downloader import (
     download,
@@ -8,67 +15,87 @@ from core.downloader import (
 from core.spotify_auth import auth, load_token
 from core.locale import get_text
 from core.utils import delete_message, is_spotify_url, send_message
-import telebot
-import time
 
 
-def register_commands(bot):
+def require_token(bot: telebot.TeleBot) -> bool:
+    """
+    Checks if a valid Spotify token exists, sending an error message if it does not.
+    """
+    token = load_token()
+    if not token:
+        send_message(bot, message=get_text("error_no_valid_token"))
+        return False
+    return True
+
+
+def register_commands(bot: telebot.TeleBot):
+    """
+    Registers all available commands in the Telegram bot.
+    """
+
+    # --- Basic commands ---
     @bot.message_handler(commands=["start"])
     def start_command(message):
+        """Shows the main menu."""
         send_message(bot, message=get_text("menu_main"))
 
     @bot.message_handler(commands=["authorize"])
     def authorize_command(message):
+        """Starts the Spotify authorization process."""
         auth(bot, message)
 
     @bot.message_handler(commands=["download"])
     def download_command(message):
+        """Requests a Spotify URL to download."""
         send_message(bot, message=get_text("download_prompt_url"))
 
+    # --- Downloads ---
     @bot.message_handler(commands=["downloadliked"])
     def download_liked_command(message):
-        token = load_token()
-        if not token:
-            send_message(bot, message=get_text("error_no_valid_token"))
-            return
-        download_liked(bot)
+        """Downloads songs marked as favorites."""
+        if require_token(bot):
+            download_liked(bot)
 
     @bot.message_handler(commands=["downloadalbums"])
     def download_albums_command(message):
-        token = load_token()
-        if not token:
-            send_message(bot, message=get_text("error_no_valid_token"))
-            return
-        download_albums(bot)
+        """Downloads albums saved by the user."""
+        if require_token(bot):
+            download_albums(bot)
 
     @bot.message_handler(commands=["downloadplaylists"])
     def download_playlists_command(message):
-        token = load_token()
-        if not token:
-            send_message(bot, message=get_text("error_no_valid_token"))
-            return
-        download_playlists(bot)
+        """Downloads playlists saved by the user."""
+        if require_token(bot):
+            download_playlists(bot)
 
+    # --- Utilities ---
     @bot.message_handler(commands=["version"])
     def version_command(message):
+        """Shows the current version of the bot."""
         x = send_message(bot, message=get_text("bot_version_info", VERSION))
         time.sleep(15)
         delete_message(bot, message_id=x.message_id)
 
     @bot.message_handler(commands=["donate"])
     def donate_command(message):
+        """Shows a message to support with a donation."""
         x = send_message(bot, message=get_text("donation_message"))
         time.sleep(45)
         delete_message(bot, message_id=x.message_id)
 
+    # --- Direct URLs ---
     @bot.message_handler(func=lambda message: is_spotify_url(message.text))
     def process_direct_url(message):
+        """Processes a Spotify URL directly."""
         download(bot, message)
 
+    # --- Fallback ---
     @bot.message_handler(func=lambda message: True)
     def echo_all(message):
+        """Shows an error if the command is not recognized."""
         bot.reply_to(message, get_text("error_unknown_command"))
 
+    # --- Commands visible in Telegram ---
     bot.set_my_commands(
         [
             telebot.types.BotCommand("/start", get_text("menu_option_start")),
